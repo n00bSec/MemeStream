40ac7a9cd7069924596e43939d3f76aa7ba26a68
n00bsec <dreamist@live.unc.edu>
2017-11-29 21:28:33 -0500

    Added FirebaseAuth.

---
+++ app/src/main/java/edu/unc/web/mobile/dreamist/memestream/MainFeed.java
@@ -0,0 +1,72 @@
+package edu.unc.web.mobile.dreamist.memestream;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainFeed extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private LinearLayoutCompat social_feed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        social_feed = findViewById(R.id.social_feed);
+
+       mAuth = FirebaseAuth.getInstance();
+       if(mAuth.getCurrentUser() == null){
+           Snackbar.make(social_feed, "Not logged in!",
+                   Snackbar.LENGTH_SHORT)
+           .setAction("Something", null).show();
+           return;
+       }
+
+       Snackbar.make(social_feed,
+               "Logged in as:" + mAuth.getCurrentUser().getDisplayName(),
+               Snackbar.LENGTH_SHORT)
+               .show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
