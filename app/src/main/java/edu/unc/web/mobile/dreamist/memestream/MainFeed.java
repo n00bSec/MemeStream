fadecb9c6d8e6e0804907e57049fa0b5c7915ae9
n00bsec <dreamist@live.unc.edu>
2017-11-29 22:50:36 -0500

    Reformat and clean after implementing login and account creation.

--- app/src/main/java/edu/unc/web/mobile/dreamist/memestream/MainFeed.java
+++ app/src/main/java/edu/unc/web/mobile/dreamist/memestream/MainFeed.java
@@ -1,17 +1,15 @@
 package edu.unc.web.mobile.dreamist.memestream;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
