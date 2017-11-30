608aa78b438e034aa375b3a4326e81dec1f8fdc7
n00bsec <dreamist@live.unc.edu>
2017-11-29 22:48:52 -0500

    Add account creation and login.

--- app/src/main/java/edu/unc/web/mobile/dreamist/memestream/MainFeed.java
+++ app/src/main/java/edu/unc/web/mobile/dreamist/memestream/MainFeed.java
@@ -1,52 +1,120 @@
 package edu.unc.web.mobile.dreamist.memestream;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainFeed extends AppCompatActivity {
 
+    private final String TAG = "MEMESTREAM";
+
+    private Toolbar toolbar;
+
+    EditText email_input, password_input;
+
     private FirebaseAuth mAuth;
     private LinearLayoutCompat social_feed;
+    private LinearLayoutCompat login_details;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar =  findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         social_feed = findViewById(R.id.social_feed);
+        email_input = findViewById(R.id.email);
+        password_input = findViewById(R.id.password);
 
-       mAuth = FirebaseAuth.getInstance();
-       if(mAuth.getCurrentUser() == null){
-           Snackbar.make(social_feed, "Not logged in!",
-                   Snackbar.LENGTH_SHORT)
-           .setAction("Something", null).show();
-           return;
-       }
+        mAuth = FirebaseAuth.getInstance();
+        login_details = findViewById(R.id.login_details);
+    }
 
-       Snackbar.make(social_feed,
-               "Logged in as:" + mAuth.getCurrentUser().getDisplayName(),
-               Snackbar.LENGTH_SHORT)
-               .show();
+    protected void onStart(){
+        super.onStart();
+        //Check if user is signed in
+        if(mAuth.getCurrentUser() == null){
+            Snackbar.make(social_feed, "Not logged in!",
+                    Snackbar.LENGTH_SHORT).show();
+            return;
+        }
+
+        Snackbar.make(social_feed,
+                "Logged in as:" + mAuth.getCurrentUser().getDisplayName(),
+                Snackbar.LENGTH_SHORT)
+                .show();
+
+        initFeed(mAuth.getCurrentUser());
+    }
+
+    public void createAccount(View v){
+        String email_str = email_input.getText().toString();
+        String password_str = password_input.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email_str, password_str)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "Login successful.");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            initFeed(user);
+                        } else {
+                            Log.w(TAG, "Login failed.");
+                            Toast.makeText(MainFeed.this,
+                                    "Login failed.",
+                                    Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    }
+                });
+    }
+
+    public void signIn(View v){
+        String email_str = email_input.getText().toString();
+        String password_str = password_input.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email_str, password_str)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "Login successful.");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            initFeed(user);
+                        } else {
+                            Log.w(TAG, "Login failure.");
+                            Toast.makeText(MainFeed.this,
+                                    "Login failure", Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    }
+                });
+    }
+
+    protected void initFeed(FirebaseUser user){
+        login_details.setVisibility(View.GONE);
+        toolbar.setTitle("Memer:" + user.getUid());
+        setSupportActionBar(toolbar);
+
     }
 
     @Override
